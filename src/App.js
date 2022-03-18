9edfc709eab6bce8593b464e4e23748606f50c4c
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-03-18 16:09:54 +0600

    react component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const student = {name : "mehedi", roll: "2850"};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* call component */}
+      {/* <Person></Person>
+      <Person></Person>
+      <Person></Person> */}
     </div>
   );
 }
 
+// create component
+/*
+function Person(){
+  return (
+    <div>
+      <h2>This is mehedi hasan</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, maiores?</p>
+    </div>
+  )
+}
+*/
 export default App;
