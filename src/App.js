364b72483cd16953bd1c5adec94d1b1945d651b6
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-03-18 17:29:26 +0600

    props

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 
 const student = {name : "mehedi", roll: "2850"};
 function App() {
+  const names = ["sakib", "tamim", "musfiq", "mehedi"];
   return (
     <div className="App">
-
+      {
+        names.map( name => <Student name = {name}></Student>)
+      }
       {/* call component */}
       {/* <Person></Person>
       <Person></Person>
       <Person></Person> */}
+      {/* <Student name ="Akkas ALi" Roll ="69" dress ="blue"></Student>
+      <Student name ="Pakir ALi" Roll ="49" dress ="black"></Student> */}
     </div>
   );
 }
@@ -25,4 +30,15 @@ function Person(){
   )
 }
 */
+// data load dynamically : component with parameter
+// in props we get data like object 
+function Student (props){
+  // console.log(props);
+  return (
+    <div>
+      <h2>Student Name : {props.name}</h2>
+      {/* <h4>Roll : {props.Roll}</h4> */}
+    </div>
+  )
+}
 export default App;
